0db545b506ab40fcf2a502d02efe46b617edb36f
Bunleekh <165245252+Bunleekh@users.noreply.github.com>
2024-03-28 06:40:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/signal', methods=['POST'])
 def receive_signal():
-    # Perform actions upon receiving the signal
-    print("Signal received from local app!")
-    return "Signal received successfully", 200
+    data = request.json  # Extract JSON data from the request
+    message = data.get('message')  # Get the 'message' field from the JSON data
+    print("Received message:", message)
+    return jsonify({"status": "success", "message": "Signal received successfully"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
